0ee7a04b0b06be23dee2ad58cacb3e6c44d64fd1
Vignesh <vignesh.raja@cioxhealth.com>
2023-09-22 11:12:45 +0530

    update rotate

--- src/Viewer/Viewer.tsx
+++ src/Viewer/Viewer.tsx
@@ -1,42 +1,79 @@
-import WebViewer, { Core } from '@pdftron/webviewer';
-import { useEffect, useRef, useState } from "react";
+import WebViewer from '@pdftron/webviewer';
+import { useEffect, useRef } from "react";
 import { Col, Container, Row } from 'react-bootstrap';
-import { LoadViewerDetails } from '../Components/LoadViewerDetails';
 import { useDispatch } from 'react-redux';
 import { selectedThumbnails, totalThumbailPages } from '../Store/Reducers/ThumbnailPages';
+import { LoadViewerDetails } from '../components/LoadViewerDetails';
 
 export const Viewer = () => {
     const viewer = useRef(null);
     const path = "/assets/multiPage.pdf";
     const dispatch = useDispatch();
-    let [selectedThumbanailPages, setSelectedThumbnailPages] = useState([]);
-
-    useEffect(() => {
-        // console.log(selectedThumbanailPages)
-        dispatch(selectedThumbnails([...selectedThumbanailPages]))
-    }, [selectedThumbanailPages]);
 
     useEffect(() => {
         if (viewer.current) {
             WebViewer({
                 path: "/lib",
                 initialDoc: path,
-                preloadWorker: `${WebViewer.WorkerTypes.PDF}`
+                disabledElements: ['viewControlsButton', 'menuButton', 'ribbons', 'panToolButton', 'selectToolButton', 'toolsHeader', 'toggleNotesButton', 'searchButton', 'documentControl'],
             },
                 viewer.current
             ).then((instance) => {
-                const { documentViewer } = instance.Core;
-                const UIEvents = instance.UI.Events;
-                // const keys = instance.UI.AnnotationKeys;
-                instance.UI.addEventListener(UIEvents.DOCUMENT_LOADED, e => {
+                const { UI, Core } = instance;
+                const { documentViewer, annotationManager, Tools, Annotations, Document } = Core;
+                const rotation = instance.Core.PageRotation;
+
+
+                const UIEvents = UI.Events;
+                const DocumentViewerEvents = Core.DocumentViewer.Events;
+                UI.addEventListener(UIEvents.DOCUMENT_LOADED, () => {
                     dispatch(totalThumbailPages(+documentViewer.getPageCount()));
                 })
-                instance.UI.addEventListener(UIEvents.SELECTED_THUMBNAIL_CHANGED, e => {
-                    setSelectedThumbnailPages(e.detail)
+                UI.addEventListener(UIEvents.SELECTED_THUMBNAIL_CHANGED, e => {
+                   dispatch(selectedThumbnails([... e.detail]))
                 })
-                documentViewer.addEventListener("rotationUpdated", (rotation : any, pageNumber : number) => {
-                    console.log(documentViewer.getRotation(2))
-                    console.log("rotaion of the page", rotation, pageNumber)
+                UI.setHeaderItems(header => {
+                    header.update([
+                        {
+                            type: 'toggleElementButton',
+                            img: 'icon-header-sidebar-line',
+                            element: 'leftPanel',
+                            dataElement: 'leftPanelButton',
+                            title: 'component.leftPanel'
+                        },
+                        {
+                            type: 'customElement',
+                            render: () => {
+                                const clockwise = document.createElement('img');
+                                clockwise.src = '/icons/arrow-clockwise.svg';
+                                clockwise.style.width = '25px';
+                                clockwise.style.marginLeft = '10px';
+                                clockwise.style.cursor = 'pointer';
+                                clockwise.onclick = () => {
+                                    const doc = documentViewer.getDocument();
+                                    doc.rotatePages(UI.ThumbnailsPanel.getSelectedPageNumbers(), rotation.E_90)
+                                }
+                                return clockwise;
+                            },
+                            title: 'clockwise'
+                        },
+                        {
+                            type: 'customElement',
+                            render: () => {
+                                const counterclockwise = document.createElement('img');
+                                counterclockwise.src = '/icons/arrow-counterclockwise.svg';
+                                counterclockwise.style.width = '25px';
+                                counterclockwise.style.marginLeft = '10px';
+                                counterclockwise.style.cursor = 'pointer';
+                                counterclockwise.onclick = () => {
+                                    const doc = documentViewer.getDocument();
+                                    doc.rotatePages(UI.ThumbnailsPanel.getSelectedPageNumbers(), rotation.E_270)
+                                }
+                                return counterclockwise;
+                            },
+                            title: 'counterclockwise'
+                        }
+                    ])
                 })
             });
         }
